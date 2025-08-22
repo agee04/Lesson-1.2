6f1e5b52302e21a7640a792d978b80f4d994b1b6
Alyssa Gee <81585824+agee04@users.noreply.github.com>
2025-08-22 14:25:45 -0700

    Create Marco.java

---
+++ Marco.java
@@ -0,0 +1,18 @@
+/*
+AP CS A
+Name: [FIRST AND LAST NAME]
+Date: [CURRENT DATE]
+Teacher: Ms. Gee
+Assignment: Project 0 - Hello World!
+Description: [WRITE ANYTHING APPROPIATE HERE.]
+*/
+ 
+public class Marco {
+    public static void main(String[] args) {
+ 
+        int i = 0; // this is and int, it stores integeres
+      
+      
+        
+    }
+}
